55fda1cbe7337e998ea0657cb9c8d3ed383c767e
navreet10 <kaur.navreet1090@gmail.com>
2016-07-26 11:41:13 -0400

    Navreet PendingActions

---
+++ src/model/HdzEmployee.java
@@ -0,0 +1,74 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the HDZ_EMPLOYEE database table.
+ * 
+ */
+@Entity
+@Table(name="HDZ_EMPLOYEE")
+@NamedQuery(name="HdzEmployee.findAll", query="SELECT h FROM HdzEmployee h")
+public class HdzEmployee implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="HDZ_EMPLOYEE_EMPLOYEEID_GENERATOR", sequenceName="HDZ_EMPLOYEE_ID_SEQ", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="HDZ_EMPLOYEE_EMPLOYEEID_GENERATOR")
+	private long employeeid;
+
+	private String email;
+
+	private String empname;
+
+	@Column(name="\"POSITION\"")
+	private String position;
+
+	private String pwd;
+
+	public HdzEmployee() {
+	}
+
+	public long getEmployeeid() {
+		return this.employeeid;
+	}
+
+	public void setEmployeeid(long employeeid) {
+		this.employeeid = employeeid;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmpname() {
+		return this.empname;
+	}
+
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+
+	public String getPosition() {
+		return this.position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getPwd() {
+		return this.pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
