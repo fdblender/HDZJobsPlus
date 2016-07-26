be7005e45a3f62b5329f5f6b4b0a1cedb2f1601b
brooney27 <brooney278@gmail.com>
2016-07-26 13:13:12 -0400

    applicant dao finished?

--- src/dao/ApplicantDao.java
+++ src/dao/ApplicantDao.java
@@ -1,11 +1,15 @@
 package dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 
 import util.DBUtil;
 import model.HdzApplicant;
 import model.HdzEducation;
+import model.HdzJob;
 import model.HdzJobhistory;
 import model.HdzReftable;
 
@@ -65,4 +69,40 @@ public class ApplicantDao {
 			em.close();
 		}
 	}
+	
+	public static List<HdzJob> getAllJobs (){
+        EntityManager em = DBUtil.getEmfFactory().createEntityManager();
+        String qString = "select j from HdzJob j";
+        
+        List<HdzJob> jobs = null;
+        try{
+            TypedQuery<HdzJob> query = em.createQuery(qString,HdzJob.class);
+            jobs = query.getResultList();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally{
+                em.close();
+            }
+        return jobs;
+    }
+	
+	public static List<HdzJob> searchJobs (String search)
+    {
+        EntityManager em = DBUtil.getEmfFactory().createEntityManager();
+        List<HdzJob> searchposts = null;
+        String qString = "select j from HdzJob j "
+                + "where j.posttext like :search";
+        
+        try{
+            TypedQuery<HdzJob> query = em.createQuery(qString,HdzJob.class);
+            query.setParameter("search", "%" + search + "%");
+            searchposts = query.getResultList();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            em.close();
+        }return searchposts;
+    }
 }
